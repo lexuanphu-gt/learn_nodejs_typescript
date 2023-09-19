a81c2204cc991e66e603142a76436f629d89d9e6
lexuanphu-gt <lexuanphugt@gmail.com>
2023-09-19 22:39:47 +0700

    update thêm API

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-const name: string = 'Le Xuan Phu'
-console.log(name)
+// const name: string = 'Le Xuan Phu'
+// console.log(name)
+// import dotenv from 'dotenv'
+// dotenv.config()
+
+import express from 'express'
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).json({ mess: 'hello' })
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
